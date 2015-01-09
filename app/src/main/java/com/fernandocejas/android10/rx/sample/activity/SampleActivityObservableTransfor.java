8b9ff43bfcecfd8b34293c5d36c87e60d31032bb
Fernando Cejas <fcejas@gmail.com>
2015-01-09 23:33:51 +0100

    Add click events for buttons.

--- app/src/main/java/com/fernandocejas/android10/rx/sample/activity/SampleActivityObservableTransfor.java
+++ app/src/main/java/com/fernandocejas/android10/rx/sample/activity/SampleActivityObservableTransfor.java
@@ -8,12 +8,21 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import com.fernandocejas.android10.rx.sample.R;
+import com.fernandocejas.android10.rx.sample.data.DataManager;
 import rx.Subscription;
 import rx.subscriptions.Subscriptions;
 
 public class SampleActivityObservableTransfor extends Activity {
+
+  @InjectView(R.id.btn_flatMap) Button btn_flatMap;
+  @InjectView(R.id.btn_concatMap) Button btn_concatMap;
+
+  private DataManager dataManager;
 
   private Subscription subscription;
 
@@ -38,5 +47,13 @@ public class SampleActivityObservableTransfor extends Activity {
 
   private void initialize() {
     this.subscription = Subscriptions.empty();
+  }
+
+  @OnClick(R.id.btn_flatMap) void onFlatMapClick() {
+
+  }
+
+  @OnClick(R.id.btn_concatMap) void onConcatMapClick() {
+
   }
 }
